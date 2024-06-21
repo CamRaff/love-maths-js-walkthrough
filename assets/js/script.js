0135976e3ae5ed559e62132d1547e8598f26b045
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-06-21 14:45:02 +0100

    added functionality to division question

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
     })
-    
+
     runGame('addition');
 
 })
@@ -46,6 +46,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === 'division') {
+        displayDivideQuestion(num1, num2);    
     } else {
 
         alert(`Unknown game type: ${gameType}`);
@@ -93,6 +95,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, 'multiply'];
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -144,6 +148,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion() {
+function displayDivideQuestion(operand1, operand2) {
     
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '/';
+
 }
